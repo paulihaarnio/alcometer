ec64ec33482987fe1f35d6194860c4d88e2d2424
paulihaarnio <pauli.haarnio@gmail.com>
2023-02-16 13:55:41 +0200

    done

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
-import { RadioButton } from 'react-native-paper';
+import { RadioButton, Switch } from 'react-native-paper';
 import NumericInput from 'react-native-numeric-input'
 import { useState } from 'react';
 import Styles from './styles/Styles.js';
-import { Alert, Button, Image, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+import { Alert, Button, Image, Pressable, ScrollView, StyleSheet, Text, TextInput, TouchableHighlight, TouchableOpacity, View } from 'react-native';
+
 
 
 
@@ -13,21 +14,36 @@ export default function App() {
   const [hours, setHours] = useState(0);
   const [weigth, setWeigth] = useState(0);
   const [result, setResult] = useState(0);
+  const [isOn, setIsOn] = useState(false);
+  const colorTheme = isOn ? Styles.darktheme : Styles.ligththeme;
+  const ButtonTheme = isOn ? Styles.DarkButton : Styles.Button;
+  const resultTheme= result<0.6 ? Styles.aresult :1.5>result>0.6 ? Styles.bresult :Styles.cresult
   function calculate(){
     let grams=(bottles*0.33)*4.5*8
     let burn = weigth/10
     let gramsleft=grams-burn*hours
-    if(radioval=='male'){
-      setResult(gramsleft/(weigth*0.7))
-    }else{
-      setResult(gramsleft/(weigth*0.6))
-    }
+    let res;
+    if(weigth>0){
+
+      if(radioval=='male'){
+        res=gramsleft/(weigth*0.7)
+      }else{
+        res=gramsleft/(weigth*0.6)
+      }
+      if(res<0){
+        res=0
+      }
+      setResult(res)
+    }else
+    Alert.alert("Incorrect weigth","Please put a correct weigth")
   }
   
   return (
-    <ScrollView>
-      <View style={Styles.container}>
-        <Text style={Styles.header}>Alcometer</Text>
+    <ScrollView style={[Styles.all, colorTheme]}>
+      <View style={[Styles.container,colorTheme]}>
+        <Text>On/Off</Text>
+        <Switch value={isOn} onValueChange={newValue=>setIsOn(newValue)}></Switch>
+        <Text style={Styles.header}>Alcometer🍻🍻🍻</Text>
         <Text style={Styles.label}>Weigth:</Text>
         <TextInput 
           keyboardType='number-pad' 
@@ -37,10 +53,12 @@ export default function App() {
         />
 
         <Text style={Styles.label}>Bottles:</Text>
-        <NumericInput value={bottles} onChange={bottles => setBottles(bottles)} minValue={0}/>
+        <View style={Styles.NumericInput}>
+        <NumericInput value={bottles} onChange={bottles => setBottles(bottles)} minValue={0} />
+        </View>
 
         <Text style={Styles.label}>Hours:</Text>
-        <NumericInput value={hours} onChange={hours => setHours(hours)} minValue={0}/>
+        <NumericInput value={hours} onChange={hours => setHours(hours)} minValue={0} style={Styles.NumericInput}/>
 
 
         <RadioButton.Group onValueChange={newValue=>setRadioval(newValue)} value={radioval}>
@@ -54,8 +72,11 @@ export default function App() {
           </View>
         </RadioButton.Group>
 
-        <Text style={Styles.result}>{result.toFixed(2)}</Text>
-        <Button title='Calculate' style={Styles.button} onPress={calculate}/>
+        <Text style={resultTheme}>{result.toFixed(2)}</Text>
+
+        <Pressable style={ButtonTheme} onPress={calculate}>
+          <Text style={Styles.ButtonText}>Calculate</Text>
+        </Pressable>
         <StatusBar style="auto" />
       </View>
     </ScrollView>
--- styles/Styles.js
+++ styles/Styles.js
@@ -1,36 +1,68 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+    all:{
+       backgroundColor:'#e3d6d5' 
+    },
     container: {
         flex: 1,
         alignItems: 'center',
         justifyContent: 'space-evenly',
-        margin: 10,
-        marginTop: 50
+        margin: 100
     },
     textInput:{
         padding: 5,
         borderWidth:1,
         marginBottom: 10,
-        width:200
+        width:200,
+        borderRadius:30,
+        backgroundColor:'white'
     },
     label:{
         fontSize: 18,
+        fontWeight: "bold",
+        color:'#84ade3'
+    },
+    aresult:{
+        color: 'green',
+        fontSize: 68,
         fontWeight: "bold"
     },
-    result:{
-        color: '#b00000',
-        fontSize: 18,
+    bresult:{
+        color: 'yellow',
+        fontSize: 68,
+        fontWeight: "bold"
+    },
+    cresult:{
+        color: 'red',
+        fontSize: 68,
         fontWeight: "bold"
     },
      radioStyle:{
         flexDirection: 'row',
-         alignItems: 'center'
+        alignItems: 'center'
     },
     header:{
-        fontSize: 40
+        fontSize: 40,
+        fontWeight:'bold',
+        color:'green'
     },
-    button:{
-        borderRadius: 100
-    }
+    Button:{
+        borderRadius:10,
+        backgroundColor:'#6f490f'
+    },
+    ButtonText:{
+        fontSize:40,
+        padding:10
+    },
+    DarkButton:{
+        borderRadius:10,
+        backgroundColor:'#f4e9d9'
+    },
+    NumericInput:{
+        borderRadius:100,
+    },
+    darktheme:{
+        backgroundColor:'#694303',
+    },
 });
